3753c1ec6e4f2cdc3ee0aa41abdd340c01606165
Michael Hoffmeister <michael.hoffmeister.hka@gmail.com>
2025-02-26 20:48:05 +0100

    * update

--- src/heos-remote/heos-remote-systray/Program.cs
+++ src/heos-remote/heos-remote-systray/Program.cs
@@ -1,22 +1,46 @@
 using CommandLine;
+using CommandLine.Text;
 using heos_remote_lib;
 
 namespace heos_remote_systray
 {
     internal static class Program
     {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
+        public static void RegisterGUIConsoleWriter()
+        {
+            AttachConsole(ATTACH_PARENT_PROCESS);
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static async Task<int> Main(string[] args)
         {
-            Task.Yield();
+            await Task.Yield();
+
+            RegisterGUIConsoleWriter();
 
             // parse options (do not care about errors)
-            var result = Parser.Default.ParseArguments<HeosAppOptions>(args);
+            var parser = new Parser(config => {
+                config.HelpWriter = null;
+            });
+            var result = parser.ParseArguments<HeosAppOptions>(args);
+
             if (result.Value == null)
+            {
+                Console.WriteLine(HelpText.AutoBuild(result, h => {
+                    h.AdditionalNewLineAfterOption = false;
+                    h.Heading = "heos-remote-systray"; //change header
+                    h.Copyright = "(C) 2025 by Michael Hoffmeister. MIT license."; //change copyright text
+                    return h;
+                }));
                 return -1;
+            }
 
             OptionsSingleton.Curr = result.Value;
 
